4df4b42a089e9ec1ebd4b148abf586a584f4b70b
Esau Matias <esaufreirematias@gmail.com>
2022-06-14 15:41:07 -0300

    feat: implementa pagina de login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,69 @@
 import React from 'react';
+import * as yup from 'yup';
+import { setLocale } from 'yup';
+import * as formik from 'formik';
+import { Container, Button, Form, Row, Col } from 'react-bootstrap';
 
 function Login() {
+  const { Formik } = formik
+
+  setLocale({
+    mixed: {
+      default: 'Não é válido',
+    },
+    string: {
+      min: 'Deve conter pelo menos 7 caracteres',
+    },
+  });
+
+  const schema = yup.object().shape({
+    nome: yup.string().min(7).required(),
+  });
+
   return (
-    <h1>Login</h1>
+    <Container>
+      <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        nome: '',
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        isValid,
+        errors,
+      }) => (
+        <Form noValidate onSubmit={handleSubmit}>
+          <Row className="mb-3">
+            <Form.Group
+              as={Col}
+              md="6"
+              controlId="validationFormik103"
+              className="position-relative"
+            >
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Digite seu nome"
+                name="nome"
+                value={values.nome}
+                onChange={handleChange}
+                isInvalid={!!errors.nome}
+              />
+
+              <Form.Control.Feedback type="invalid" tooltip>
+                {errors.nome}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Button type="submit" disabled={!isValid}>Submit form</Button>
+        </Form>
+      )}
+    </Formik>
+    </Container>
   )
 }
 
