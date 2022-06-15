5b6ff1d4418b5baec65cdb49e3be256ae4c9550c
Esau Matias <esaufreirematias@gmail.com>
2022-06-15 15:23:03 -0300

    feat: implementa e estiliza componente Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { Nav, Navbar, Container } from 'react-bootstrap';
+import React, { useContext } from 'react';
+// import { Nav, Navbar, Container } from 'react-bootstrap';
+import AppContext from '../../Context/AppContext';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header(props) {
   const { n } = props;
+  const { user } = useContext(AppContext);
   return (
-    <header className="containerNav">
-      <Link to='/search' className='navLink' style={{ background: n === '1' ? '#928f8f': null}}>
-        <div>Pesquisa</div>
-      </Link>
+    <header>
+      <div className="containerTop">
+        <h3>Play Music</h3>
+        <div>
+          <div><img alt="user" src="https://img.icons8.com/external-bearicons-glyph-bearicons/64/undefined/external-User-essential-collection-bearicons-glyph-bearicons.png"/></div>
+          <div>{user}</div>
+        </div>
+      </div>
 
-      <Link to='/favorites'  className='navLink' style={{ background: n === '2' ? '#928f8f': null}}>
-        <div>Favoritas</div>
-      </Link>
+      <div className="containerNav">
+        <Link to='/search' className='navLink' style={{ background: n === '1' ? '#928f8f': null}}>
+          <div>Pesquisa</div>
+        </Link>
 
-      <Link to='/profile' className='navLink' style={{ background: n === '3' ? '#928f8f': null}}>
-        <div>Perfil</div>
-      </Link>
+        <Link to='/favorites'  className='navLink' style={{ background: n === '2' ? '#928f8f': null}}>
+          <div>Favoritas</div>
+        </Link>
+
+        <Link to='/profile' className='navLink' style={{ background: n === '3' ? '#928f8f': null}}>
+          <div>Perfil</div>
+        </Link>
+      </div>
     </header>
   );
 }
